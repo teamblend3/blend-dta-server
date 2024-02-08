65bfa4afc2a6dc08c1d0d7a189f018403a5af404
백종우 <jongwoo@baegjong-uui-MacBookPro.local>
2024-02-08 22:53:33 +0900

    Refactor/add token validate condition-2

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,11 +1,7 @@
 const createHttpError = require("http-errors");
 const User = require("../models/User");
 const { COOKIE_MAX_AGE } = require("../utils/constants");
-const {
-  makeAccessToken,
-  makeRefreshToken,
-  jwtVerifyToken,
-} = require("../utils/jwtUtils");
+const { makeAccessToken, jwtVerifyToken } = require("../utils/jwtUtils");
 
 const verifyToken = async (req, res, next) => {
   try {
@@ -14,7 +10,7 @@ const verifyToken = async (req, res, next) => {
     if (!accessToken && !refreshToken) {
       req.user = null;
 
-      return next(createHttpError(401, "Unauthorized"));
+      return next();
     }
 
     const decodedAccessToken = jwtVerifyToken(accessToken);
@@ -23,6 +19,9 @@ const verifyToken = async (req, res, next) => {
     if (!accessToken && refreshToken) {
       if (decodedRefreshToken.type) {
         const findUser = await User.findById(decodedRefreshToken.id);
+
+        req.user = findUser._id;
+
         const newAccessToken = makeAccessToken(findUser._id);
 
         res.status(201).cookie("accessToken", newAccessToken, {
@@ -36,24 +35,16 @@ const verifyToken = async (req, res, next) => {
       return next(createHttpError(401, "Unauthorized"));
     }
 
-    if (decodedAccessToken.message === "jwt expired") {
+    if (
+      !decodedAccessToken.type ||
+      decodedAccessToken.message === "jwt expired"
+    ) {
       if (decodedRefreshToken.type) {
-        const newAccessToken = makeAccessToken(decodedRefreshToken._id);
+        const findUser = await User.findById(decodedRefreshToken.id);
 
-        res.status(201).cookie("accessToken", newAccessToken, {
-          maxAge: COOKIE_MAX_AGE,
-          httpOnly: true,
-        });
+        req.user = findUser._id;
 
-        return next();
-      }
-
-      return next(createHttpError(401, "Unauthorized"));
-    }
-
-    if (!decodedAccessToken.type) {
-      if (decodedRefreshToken.type) {
-        const newAccessToken = makeAccessToken(decodedRefreshToken._id);
+        const newAccessToken = makeAccessToken(findUser._id);
 
         res.status(201).cookie("accessToken", newAccessToken, {
           maxAge: COOKIE_MAX_AGE,
@@ -68,10 +59,12 @@ const verifyToken = async (req, res, next) => {
 
     if (
       !decodedRefreshToken.type ||
-      decodedRefreshToken.message === "jwt expired"
+      decodedRefreshToken.message === "expired jwt"
     ) {
       return next(createHttpError(401, "Unauthorized"));
     }
+
+    req.user = decodedAccessToken.id;
 
     return next();
   } catch (error) {
