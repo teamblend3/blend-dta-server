0e43b341390e5084e4d2afbb24d7031ecefa4fbb
kimabraham <kimkr88@icloud.com>
2024-02-14 00:42:55 +0900

    Chore: aws s3 package-module upgrade

--- utils/aws.js
+++ utils/aws.js
@@ -1,8 +1,11 @@
-const AWS = require("aws-sdk");
+const { S3Client } = require("@aws-sdk/client-s3");
+const { Upload } = require("@aws-sdk/lib-storage");
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+const s3Client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
   region: process.env.AWS_REGION,
 });
 
@@ -11,12 +14,17 @@ const uploadFileToS3 = async file => {
 
   const uploadParams = {
     Bucket: process.env.AWS_S3_BUCKET_NAME,
-    Key: `uploads/${name}`, // 한글 파일 이름을 그대로 사용
+    Key: `uploads/${name}`,
     Body: buffer,
   };
 
   try {
-    const uploadResult = await s3.upload(uploadParams).promise();
+    const uploader = new Upload({
+      client: s3Client,
+      params: uploadParams,
+    });
+
+    const uploadResult = await uploader.done();
     return uploadResult;
   } catch (error) {
     console.error("Error uploading file to S3", error);
