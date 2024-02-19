5967d4609113fde6f05eaaacd0b5d39b6ab52e69
백종우 <jongwoo@baegjong-uui-MacBookPro.local>
2024-02-19 19:58:45 +0900

    response with dupilcated projects

--- utils/constants.js
+++ utils/constants.js
@@ -1,7 +1,6 @@
 const COOKIE_MAX_AGE = 4 * 60 * 60 * 1000;
 const ACCESS_TOKEN_EXPIRES_IN = "1h";
 const REFRESH_TOKEN_EXPIRES_IN = "15d";
-const GOOGLE_SHEET_SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 const SALT_ROUNDS = 10;
 const ITEMS_PER_PAGE = 5;
 const GOOGLE_SHEET_SCOPES = [
@@ -9,6 +8,7 @@ const GOOGLE_SHEET_SCOPES = [
   "https://www.googleapis.com/auth/userinfo.email",
   "https://www.googleapis.com/auth/userinfo.profile",
 ];
+const REDIRECT_URI = "http://localhost:5173";
 const STATUS_MESSAGE = {
   CONNECTED: "CONNECTED_DB_DONE",
   FETCHED: "FETCH_DATA_DONE",
@@ -21,6 +21,7 @@ module.exports = {
   ACCESS_TOKEN_EXPIRES_IN,
   REFRESH_TOKEN_EXPIRES_IN,
   GOOGLE_SHEET_SCOPES,
+  REDIRECT_URI,
   SALT_ROUNDS,
   ITEMS_PER_PAGE,
   STATUS_MESSAGE,
